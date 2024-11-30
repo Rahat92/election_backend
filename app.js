2ecf2530cd4e5f15db07c7b3a75dc1d44d54fdda
Rahat92 <khrahat92@gmail.com>
2024-11-30 16:00:23 +0600

    server test

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ app.use(express.json());
 app.use(cors())
 app.get('/favicon.ico', (req, res) => res.status(204));
 
+app.get('/', (req,res) => {
+    res.status(200).json({
+        status: 'Success',
+        message: 'Surver is up and running'
+    })
+})
+
 app.use('/api/v1/all-issued-member',issuedMemberRouter);
 app.use('/api/v1/project',projectRouter);
 app.use('/api/v1/voter-slip',voterSlipIssueRouter);
@@ -26,6 +33,7 @@ app.use('/api/v1/all-members',memberRouter);
 app.use('/api/v1/report',reportRouter);
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/result', resultRouter)
+
 app.all('*', (req,res, next) => {
     // return next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
     res.status(404).json({
